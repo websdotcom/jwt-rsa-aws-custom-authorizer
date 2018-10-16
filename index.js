08079b553acccd060a72d0f2558480e0ff6b2ac8
mmaddex <matt.maddex@auth0.com>
2018-10-16 15:31:10 -0700

    use return a 401 status code

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const lib = require('./lib');
 let data;
 
 // Lambda function index.handler - thin wrapper around lib.authenticate
-module.exports.handler = async (event) => {
+module.exports.handler = async (event, context, callback) => {
   try {
     data = await lib.authenticate(event);
   }
   catch (err) {
       console.log(err);
-      return `Unauthorized: ${err.message}`;
+      return context.fail("Unauthorized");
   }
   return data;
 };
